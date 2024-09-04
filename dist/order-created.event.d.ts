f136e69adb5b1e9383f10a9a1bc9921997be762e
Shaheer-Moidin-SB <moidinsb24@gmail.com>
2024-09-04 15:01:14 +0400

    Delete dist directory
    deleted dist folder

--- dist/order-created.event.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class OrderCreatedEvent {
-    readonly orderId: string;
-    readonly userId: string;
-    readonly price: number;
-    constructor(orderId: string, userId: string, price: number);
-    toString(): string;
-}
